18cc102aaad6b174e85a716112e2e899fe68fdfe
Stephani Gonzalez <spgonza@live.com>
2017-01-20 13:30:23 -0700

    first commit

---
+++ freelance_camp_documents/db/seeds.rb
@@ -0,0 +1,9 @@
+10.times do |d|
+	FreelanceDocument.create!(
+		title: "Document #{d}",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+		file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+		image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+	)
+end
+
